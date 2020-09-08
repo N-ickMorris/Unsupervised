f6201b1c2a6121ece6dce16703571ac17688e489
Nicholas J Morris <nicholasjmorris93@gmail.com>
2020-09-08 16:02:25 -0400

    Add files via upload

--- ansur_isomap.py
+++ ansur_isomap.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Reduces the columns of a data set with a Principal Component Analysis model
+Reduces the columns of a data set with an Isomap model
 
 @author: Nick
 """
@@ -8,7 +8,7 @@ Reduces the columns of a data set with a Principal Component Analysis model
 
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import PCA
+from sklearn.manifold import Isomap
 from sklearn.ensemble import RandomForestRegressor
 from plots import scatter_plot
 
@@ -24,18 +24,18 @@ np.random.seed(1)
 test_idx = np.random.choice(a=X.index.values, size=int(X.shape[0] / 5), replace=False)
 train_idx = np.array(list(set(X.index.values) - set(test_idx)))
 
-# train a PCA model
-n_comp = 1 # number of principal components
-component = PCA(n_components=n_comp, random_state=42)
+# train a isomap model
+n_comp = 1 # number of components
+component = Isomap(n_components=n_comp, n_neighbors=5, n_jobs=1)
 component.fit(X.iloc[train_idx, :])
 
 # compute components for all the data, add cluster labels and train/test labels
 components = pd.DataFrame(component.transform(X), 
-                          columns=["PC" + str(i + 1) for i in range(n_comp)])
+                          columns=["IC" + str(i + 1) for i in range(n_comp)])
 components["Data"] = "Train"
 for j in test_idx:
     components.loc[j, "Data"] = "Test"
-# components.to_csv("pca.csv", index=False)
+# components.to_csv("isomap.csv", index=False)
 
 # combine the data and components
 data = pd.concat([X, components], axis=1)
@@ -60,5 +60,5 @@ comp_ = 0 # the column number of a component to plot
 for c in features:
     scatter_plot(data=data,
                  x=c, y=components.columns[comp_],
-                 color=None, title="Principal Component Analysis",
+                 color=None, title="Isomap",
                  legend=True, save=False)
